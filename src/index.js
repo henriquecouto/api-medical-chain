cc395b4ed0386bec9ed5d88fca4f3f4559067e4f
Henrique Couto <henrique.coutu@live.com>
2020-02-27 23:52:39 -0300

    change bigchaindb driver to specific version to fix error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bigchaindb = require('bigchaindb-driver');
 
 const app = express();
+const BIGCHAIN_PATH = 'http://localhost:9984/api/v1/';
+const conn = new bigchaindb.Connection(BIGCHAIN_PATH);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  const assets = await conn.searchAssets('');
+  res.send(assets);
 });
 
 app.listen(3000, () => {
-  console.log('App listening on port 3000!');
+  console.log('App listening on port http://localhost:3000');
 });
