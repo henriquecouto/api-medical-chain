b19dd152e647b6c75689e873f49ef480f6390389
Henrique Couto <email@henriquecouto.com.br>
2021-04-10 15:58:13 -0300

    creates a copy of appointment data object before make transactions in bigchainDB persist

--- src/schedules/persist.js
+++ src/schedules/persist.js
@@ -1,10 +1,16 @@
 const schedule = require('node-schedule');
-const persistEthereum = require('./persistEthereum');
+const persist = require('./persistBigchainDB');
 
 module.exports = async () => {
   console.log('job scheduled');
   return schedule.scheduleJob('*/10 * * * * *', async () => {
-    const transactions = await persistEthereum.loadTransactions();
-    persistEthereum.makeTransactions(transactions);
+    try {
+      const transactions = await persist.loadTransactions();
+      if (transactions.length) {
+        persist.makeTransactions(transactions);
+      }
+    } catch (error) {
+      console.log({ error });
+    }
   });
 };
